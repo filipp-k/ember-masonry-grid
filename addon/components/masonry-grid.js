d04788e1de66dfdf7989c24ec704a1394583f883
Filipp Qoma <filippk8@gmail.com>
2015-06-10 12:15:24 +0300

    events triggering

--- addon/components/masonry-grid.js
+++ addon/components/masonry-grid.js
@@ -45,15 +45,50 @@ export default Ember.Component.extend({
     this.layoutMasonry();
   }),
 
+  events: [
+    'transitionEnd',
+    'layout',
+    'layoutComplete',
+    'remove',
+    'removeComplete'
+  ],
+  eventListeners: [],
+
   layoutMasonry: Ember.observer('items.@each', function () {
     var _this = this;
 
     imagesLoaded(this.$(), function () {
+      var options = _this.get('options');
+      var isInitLayout = options.isInitLayout || true; // save set options or default value
+      options.isInitLayout = false; // turn off initialization to set up event listeners
+      
       if (_this.get('masonryInitialized')) {
+        _this.eventListeners.map(function (obj) {
+          _this.$().masonry('off', obj.name, obj.callback);
+        });
+        _this.eventListeners = [];
         _this.$().masonry('destroy');
       }
+      // setup masonry without initialization
+      _this.$().masonry(options);
 
-      _this.$().masonry(_this.get('options'));
+      // setup event listeners
+      _this.events.map(function (event) {
+        var obj = {
+          event: event,
+          callback: function () {
+            _this.$().trigger('masonry.' + event);
+          }
+        };
+        _this.$().masonry('on', obj.event, obj.callback);
+        _this.eventListeners.push(obj);
+      });
+
+      // initiate masonry if user wants it
+      if (isInitLayout) {
+        _this.$().masonry();
+      }
+
       _this.set('masonryInitialized', true);
     });
   })
